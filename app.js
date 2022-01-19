b64fb0250a41f1d9c5033c8e410afc93650d8538
ndrozdovs <43936262+ndrozdovs@users.noreply.github.com>
2022-01-19 11:59:45 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/company', (req, res) => {
+    res.render('company');
+})
+
+app.get('/pricing', (req, res) => {
+    res.render('pricing');
+})
+
+app.get('/product', (req, res) => {
+    res.render('product');
+})
+
+app.get('/useCases', (req, res) => {
+    res.render('useCases');
+})
+
+app.get('/signIn', (req, res) => {
+    res.render('signIn');
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000');
+})
