80ea229db266579e4a7771af43a0721ead3740d8
Nikita Drozdovs <ndrozdovs@icloud.com>
2022-01-21 17:00:21 -0800

    More dhasboard changes

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,1 +1,27 @@
-console.log("IN THE SCRIPT");
+window.onload=function(){
+    $('#save').click(function() {
+        document.querySelector('#wifiModal').innerHTML += $("input[name='flexRadioDefault']:checked").val();
+    });
+
+    $('#submitWifi').click(function() {
+        document.querySelector('#startAddHub').remove();
+        document.querySelector('#firstHub').innerHTML = $("input[name='flexRadioDefault']:checked").val();
+        document.querySelector('#displayHubList').classList.toggle("hidden");
+    });
+
+    $('#addNodes').click(function() {
+        var checked = document.querySelectorAll("input[type=checkbox]:checked");
+        console.log($("#inputGroup").val());
+        document.querySelector('#firstGroup').innerHTML = $("#inputGroup").val();
+        document.querySelector('#displayGroupList').classList.toggle("hidden");
+        document.querySelector('#createGroupButton').remove();
+
+        const nodeHeader = document.querySelector('#nodeList');
+        for(node of checked){
+            const newButton = document.createElement("button");
+            newButton.innerHTML = node.value;
+            newButton.classList = "btn btn-outline-dark mx-1";
+            nodeHeader.appendChild(newButton);
+        }
+    });
+}
