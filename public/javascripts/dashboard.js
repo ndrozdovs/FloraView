a108341ce526e8a7d991d2e06b9dff6fc8aa1d6a
Nikita Drozdovs <ndrozdovs@icloud.com>
2022-01-30 12:06:10 -0800

    Added comments and display nodes when group is created

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,30 +1,39 @@
 const nodes = ['Node 1', 'Node 2', 'Node 3', 'Node 4', 'Node 5', 'Node 6']
-const groupNodeData = [];
+const groupNodeData = []; 
 
 window.onload=function(){
-    $('#save').click(function() {
+    // When Connect button is pressed in the Hub selector modal
+    $('#hubConnectButton').click(function() {
+        // Add Hub name to the next modal screen 
         document.querySelector('#wifiModal').innerHTML += $("input[name='flexRadioDefault']:checked").val();
     });
 
-    $('#submitWifi').click(function() {
+    // When Wifi credentials are submitted
+    $('#submitWifiButton').click(function() {
+        // Enable the spinner
         document.querySelector('#wifiSpinner').classList.remove("hidden");
 
+        // Wait 2 seconds and then close the modal
         window.setTimeout(function () {
             $('#enterWifiModal').modal('hide');
-            document.querySelector('#startAddHub').remove();
-            document.querySelector('#firstHub').innerHTML = $("input[name='flexRadioDefault']:checked").val();
-            document.querySelector('#displayHubList').classList.remove("hidden");
-            document.querySelector('#wifiSpinner').remove();
+            document.querySelector('#startAddHub').remove(); // Remove initial set up info
+            document.querySelector('#firstHub').innerHTML = $("input[name='flexRadioDefault']:checked").val(); // Save selected Hub
+            document.querySelector('#displayHubList').classList.remove("hidden"); // Display the next info screen
+            document.querySelector('#wifiSpinner').remove(); // Disbale the spinner
         }, 2000);
     });
 
-    $('#addNodes').click(function() {
-        var checked = document.querySelectorAll("input[type=checkbox]:checked");
-        document.querySelector('#displayGroupList').classList.remove("hidden");
+    // When new group is created
+    $('#submitGroupButton').click(function() {
+        var checked = document.querySelectorAll("input[type=checkbox]:checked"); // Selected nodes (checkboxes)
+        document.querySelector('#displayGroups_Nodes').classList.remove("hidden"); // Display groups and nodes screen
+
+        // Remove "Create a student group" button if it exists
         if(document.contains(document.querySelector('#createGroupButton'))){
             document.querySelector('#createGroupButton').remove();
         }
 
+        // Create new group button and bind populateNodes function
         const groupHeader = document.querySelector('#groupList');
         const newGroup = document.createElement("button");
         newGroup.classList = "btn buttonClr mx-1";
@@ -32,27 +41,32 @@ window.onload=function(){
         newGroup.innerHTML = $("#inputGroup").val();
         groupHeader.appendChild(newGroup);
         newGroup.onclick = function() {populateNodes(newGroup)};
-        groupNodeData[newGroup.id] = [];
+        groupNodeData[newGroup.id] = []; // Add new group to group node data
 
-        const nodeHeader = document.querySelector('#nodeList');
+        // Add selected nodes to the respective group and remove those nodes from available nodes
         for(node of checked){
             groupNodeData[newGroup.id].push(node.value);
             nodes.splice(nodes.indexOf(node.value), 1);
         }
 
-        document.querySelector('#inputGroup').value = "";
+        document.querySelector('#inputGroup').value = ""; // Reset modal text
+        populateNodes(newGroup);
     });
 
-    $("#createGroup").on('shown.bs.modal', function(){
-        const nodeHeader = document.querySelector('#nodeListForm');
+    // When create a new group modal is on screen
+    $("#createGroupModal").on('shown.bs.modal', function(){
+        const nodeHeader = document.querySelector('#nodeListForm'); // Select node list in the modal
+
+        // Remove all nodes from the list (clear the list)
         var child = nodeHeader.lastElementChild; 
         while (child) {
             nodeHeader.removeChild(child);
             child = nodeHeader.lastElementChild;
         }
 
+        // Populate the list
         for(node of nodes){
-            const newDiv = document.createElement("div");
+            const newDiv = document.createElement("div"); 
             const newInput = document.createElement("input");
             const newLabel = document.createElement("label");
 
@@ -71,23 +85,28 @@ window.onload=function(){
         }
     });
 
-    function populateNodes(newGroup) {
-        const nodeHeader = document.querySelector('#nodeList');
+    // Associate respective group and node pairings and display them 
+    function populateNodes(selectedGroup) {
+        const nodeHeader = document.querySelector('#nodeList'); // Select nodes currently being shown
+
+        // Remove all nodes currently being displayed
         var child = nodeHeader.lastElementChild; 
         while (child) {
             nodeHeader.removeChild(child);
             child = nodeHeader.lastElementChild;
         }
 
-        for(node of groupNodeData[newGroup.id]){
+        // Create new node list based on groupNodeData map
+        for(node of groupNodeData[selectedGroup.id]){
             const newNode = document.createElement("button");
             newNode.innerHTML = node;
             newNode.classList = "btn btn-outline-dark mx-1";
             nodeHeader.appendChild(newNode);
         }
 
+        // Highlight the currently selected group
         for(const key of Object.keys(groupNodeData)){
-            if(newGroup.id === key){
+            if(selectedGroup.id === key){
                 document.querySelector('#' + key).classList.add('highlight');
             }
             else{
