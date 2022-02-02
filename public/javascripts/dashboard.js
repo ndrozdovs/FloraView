01bc1da26627fb62a1cc50f81adfa61113022364
Nikita Drozdovs <ndrozdovs@icloud.com>
2022-02-01 16:06:46 -0800

    Added calendar range for graph data

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -31,6 +31,7 @@ window.onload=function(){
         // Remove "Create a student group" button if it exists
         if(document.contains(document.querySelector('#createGroupButton'))){
             document.querySelector('#createGroupButton').remove();
+            document.querySelector('#groupText').remove();
         }
 
         // Create new group button and bind populateNodes function
@@ -83,6 +84,30 @@ window.onload=function(){
             newDiv.appendChild(newInput);
             newDiv.appendChild(newLabel);
         }
+    });
+
+    $(function() {
+        var start = moment().subtract(0, 'days');
+        var end = moment();
+    
+        function cb(start, end) {
+            updateTimeScale(start.format('YYYY-MM-DD'), end.format('YYYY-MM-DD'))
+            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+    
+        $('#reportrange').daterangepicker({
+            startDate: start,
+            endDate: end,
+            ranges: {
+               'Today': [moment(), moment()],
+               'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+               'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+               'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+               'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, cb);
+
+        cb(start, end);
     });
 
     // Associate respective group and node pairings and display them 
--- public/javascripts/graphs.js
+++ public/javascripts/graphs.js
@@ -13,8 +13,8 @@ for(var i = 0; i < 4; i++){
       scales: {
         x: {
           parsing: false,
-          min: '2021-01-31 00:00:00',
-          max: '2021-02-01 00:00:00',
+          min: '2022-02-01 00:00:00',
+          max: '2022-02-01 24:00:00',
           type: 'time',
           time: {
             unit: 'hour'
@@ -42,25 +42,15 @@ chartElement = ['temperatureChart', 'phChart', 'lightChart', 'moistureChart']
 minValue = [15, 4 , 30, 50]
 maxValue = [25, 10, 60, 100]
 
-randomizeGraphs();
+initGraphs();
 
-function randomizeGraphs(){
+function initGraphs(){
   for(var i = 0; i < 4; i++){
     configs[i].data.datasets[0].label = labels[i];
     configs[i].data.datasets[0].backgroundColor = colors[i];
     configs[i].data.datasets[0].borderColor = colors[i];
     configs[i].data.datasets[0].data = [];
-  
-    for(var j = 0; j < 18; j++){
-      if(j < 10){
-        time = `0${j}`;
-      }
-      else{
-        time = j
-      }
-      configs[i].data.datasets[0].data.push({x: `2021-01-31 ${time}:00:00`, y: getRandomInt(minValue[i], maxValue[i])})
-    }
-    
+
     charts.push(new Chart(document.getElementById(chartElement[i]), configs[i]));
   }
 }
@@ -69,14 +59,33 @@ function updateGraphs(node){
   for(var i = 0; i < 4; i++){
     configs[i].data.datasets[0].data = [];
   
-    for(var j = 0; j < 18; j++){
+    for(var k = 1; k < 32; k++){
+      if(k < 10){
+        day = `0${k}`;
+      }
+      else{
+        day = k;
+      }
+
+      for(var j = 0; j < 25; j++){
+        if(j < 10){
+          time = `0${j}`;
+        }
+        else{
+          time = j
+        }
+        configs[i].data.datasets[0].data.push({x: `2022-01-${day} ${time}:00:00`, y: getRandomInt(minValue[i], maxValue[i])})
+      }
+    }
+
+    for(var j = 0; j < 19; j++){
       if(j < 10){
         time = `0${j}`;
       }
       else{
         time = j
       }
-      configs[i].data.datasets[0].data.push({x: `2021-01-31 ${time}:00:00`, y: getRandomInt(minValue[i], maxValue[i])})
+      configs[i].data.datasets[0].data.push({x: `2022-02-01 ${time}:00:00`, y: getRandomInt(minValue[i], maxValue[i])})
     }
 
     charts[i].update();
@@ -92,5 +101,21 @@ function updateGraphs(node){
     else{
       document.querySelector('#' + key.id).classList.remove('highlightButton');
     }
+  }
+}
+
+function updateTimeScale(start, end){
+  for(var i = 0; i < 4; i++){
+    configs[i].options.scales.x.min = start + ' 00:00:00'
+    configs[i].options.scales.x.max = end + ' 24:00:00'
+
+    if(start !== end){
+      configs[i].options.scales.x.time.unit = 'day'
+    }
+    else{
+      configs[i].options.scales.x.time.unit = 'hour'
+    }
+
+    charts[i].update();
   }
 }
