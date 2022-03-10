d0d520b8b8038a719e4026f16cf470b002666415
Nikita Drozdovs <ndrozdovs@icloud.com>
2022-03-10 12:23:34 -0800

    Fixed bugs

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,79 +1,34 @@
+const nodes = ["Node 1", "Node 2", "Node 3", "Node 4", "Node 5", "Node 6"];
+
 window.addEventListener("load", interact);
 
-async function getAllGroups() {
-  const response = await fetch("http://localhost:3000/profiles/getGroups");
-  const data = await response.json();
-
-  return data;
-}
-
-var groupButtons = document.querySelectorAll("button.custom");
-groupButtons.forEach((button) => {
-  button.addEventListener("click", async function () {
-    await getAllGroups().then((groupNodeData) => {
-      const nodeHeader = document.querySelector("#nodeList"); // Select nodes currently being shown
-      var customFlag = true;
-      var index = 0;
-
-      for(var i = 0; i < Object.keys(groupNodeData).size; i++){
-          if(this.id === groupNodeData[i].groupName){
-              break;
-          }
-          index++;
-      }
-
-      // Remove all nodes currently being displayed
-      var child = nodeHeader.lastElementChild;
-      while (child) {
-        nodeHeader.removeChild(child);
-        child = nodeHeader.lastElementChild;
-      }
-
-      // Create new node list based on groupNodeData map
-      for (node of groupNodeData[this.id]) {
-        const newNode = document.createElement("button");
-        newNode.innerHTML = node;
-        newNode.classList = "btn blackBorder shadow-none mx-1";
-        newNode.id = node.replace(/ /g, "");
-        if (customFlag) {
-          newNode.onclick = function () {
-            realtimeGraphs(newNode);
-          };
-          customFlag = false;
-        } else {
-          newNode.onclick = function () {
-            updateGraphs(newNode);
-          };
-        }
-        nodeHeader.appendChild(newNode);
-      }
-
-      const allNodes = document.createElement("button");
-      allNodes.innerHTML = "All Nodes";
-      allNodes.classList = "btn blackBorder shadow-none mx-1";
-      allNodes.id = "allNodes";
-      allNodes.onclick = function () {
-        showAllGraphs(allNodes);
-      };
-      nodeHeader.appendChild(allNodes);
-
-      // Highlight the currently selected group
-      for (const key of Object.keys(groupNodeData)) {
-        if (this.id === key) {
-          document.querySelector("#" + key).classList.add("highlight");
-        } else {
-          document.querySelector("#" + key).classList.remove("highlight");
-        }
-      }
-
-      initAllGraphs(allNodes);
-      realtimeGraphs(nodeHeader.children[0]);
+function addListenerToButtons(){
+  var groupButtons = document.querySelectorAll("button.custom");
+  groupButtons.forEach((button) => {
+    button.addEventListener("click", async function() {
+      console.log("MY ASYNC")
+      const groupId = this.id;
+      await getAllGroups().then((groupNodeData) => {
+        removeAddedNodes(groupNodeData)
+        populateNodes(groupId, groupNodeData);
+      });
     });
   });
-});
+}
+
+function removeAddedNodes(groupNodeData){
+  for (const key of Object.keys(groupNodeData)){
+    for (node of groupNodeData[key]) {
+      const index = nodes.indexOf(node)
+      if(index > -1){
+        nodes.splice(index, 1);
+      }
+    }
+  }
+}
 
 // Associate respective group and node pairings and display them
-function populateNodes(selectedGroup, groupNodeData) {
+function populateNodes(groupId, groupNodeData) {
   const nodeHeader = document.querySelector("#nodeList"); // Select nodes currently being shown
   var customFlag = true;
 
@@ -85,7 +40,7 @@ function populateNodes(selectedGroup, groupNodeData) {
   }
 
   // Create new node list based on groupNodeData map
-  for (node of groupNodeData[selectedGroup]) {
+  for (node of groupNodeData[groupId]) {
     const newNode = document.createElement("button");
     newNode.innerHTML = node;
     newNode.classList = "btn blackBorder shadow-none mx-1";
@@ -114,7 +69,7 @@ function populateNodes(selectedGroup, groupNodeData) {
 
   // Highlight the currently selected group
   for (const key of Object.keys(groupNodeData)) {
-    if (selectedGroup === key) {
+    if (groupId === key) {
       document.querySelector("#" + key).classList.add("highlight");
     } else {
       document.querySelector("#" + key).classList.remove("highlight");
@@ -147,7 +102,28 @@ function addGroupToProfile(groupName, nodes) {
   });
 }
 
-function interact() {
+async function getAllGroups() {
+  const response = await fetch("http://localhost:3000/profiles/getGroups");
+  const data = await response.json();
+
+  return data;
+}
+
+async function interact() {
+  addListenerToButtons();
+  const groups = document.querySelector("#groupList").childNodes;
+  if (groups.length !== 0) {
+    for (group of groups) {
+      if (group.tagName === "BUTTON") {
+        await getAllGroups().then((groupNodeData) => {
+          removeAddedNodes(groupNodeData)
+          populateNodes(group.id, groupNodeData)
+        });
+        break;
+      }
+    }
+  }
+
   // When new group is created
   $("#submitGroupButton").click(function () {
     var checked = document.querySelectorAll("input[type=checkbox]:checked"); // Selected nodes (checkboxes)
@@ -162,25 +138,22 @@ function interact() {
     // Create new group button and bind populateNodes function
     const groupHeader = document.querySelector("#groupList");
     const newGroup = document.createElement("button");
-    newGroup.classList = "btn blackBorder shadow-none mx-1";
+    newGroup.classList = "btn blackBorder shadow-none mx-1 custom";
     newGroup.id = $("#inputGroup").val();
     newGroup.innerHTML = $("#inputGroup").val();
     groupHeader.appendChild(newGroup);
-    newGroup.onclick = function () {
-      populateNodes(newGroup);
-    };
-    groupNodeData[newGroup.id] = []; // Add new group to group node data
+    newNodes = []; // Add new group to group node data
 
     // Add selected nodes to the respective group and remove those nodes from available nodes
     for (node of checked) {
-      groupNodeData[newGroup.id].push(node.value);
-      nodes.splice(nodes.indexOf(node.value), 1);
+      newNodes.push(node.value)
     }
 
-    addGroupToProfile(newGroup.id, groupNodeData[newGroup.id]);
+    console.log(newNodes)
+    addGroupToProfile(newGroup.id, newNodes);
 
     document.querySelector("#inputGroup").value = ""; // Reset modal text
-    populateNodes(newGroup);
+    addListenerToButtons();
   });
 
   // When create a new group modal is on screen
@@ -214,21 +187,4 @@ function interact() {
       newDiv.appendChild(newLabel);
     }
   });
-}
-
-function wifiIsInvalid() {
-  document.querySelector("#wifiSpinner").classList.toggle("hidden"); // Disable the spinner
-  document.querySelector("#inputNetworkName").value = ""; // Reset modal text
-  document.querySelector("#inputNetworkPassword").value = ""; // Reset modal text
-  document.querySelector("#inputNetworkName").classList.add("is-invalid");
-  document.querySelector("#inputNetworkPassword").classList.add("is-invalid");
-}
-
-function wifiIsValid(hubMacAddress) {
-  $("#enterWifiModal").modal("hide");
-  document.querySelector("#startAddHub").remove(); // Remove initial set up info
-  document.querySelector("#firstHub").innerHTML = hubMacAddress; // Save selected Hub
-  document.querySelector("#displayHubList").classList.remove("removed"); // Display the next info screen
-  document.querySelector("#wifiSpinner").classList.toggle("hidden"); // Disable the spinner
-  createProfile(hubMacAddress);
 }
