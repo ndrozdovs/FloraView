17a3bacee9b985f4a3b17632acd2adf4b9dbb07c
Nikita Drozdovs <ndrozdovs@icloud.com>
2022-04-01 15:59:14 -0700

    Fix

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const MongoDBStore = require("connect-mongo");
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/FloraView";
 
 const corsOptions ={
-  origin:'*', 
+  origin:'https://strawberry-custard-75142.herokuapp.com', 
   credentials:true,       
   optionSuccessStatus:200,
 }
