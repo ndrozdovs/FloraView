46ad508634fb777f3116ed8613265b37ce104a96
Nikita Drozdovs <ndrozdovs@icloud.com>
2022-04-02 14:28:49 -0700

    Force using https

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,15 @@
+if(process.env.NODE_ENV !== "production"){
+  require('dotenv').config();
+}
+
 const mongoose = require("mongoose");
 const Hub = require("../models/hub");
 const Node = require("../models/node");
 const moment = require("moment");
 
-mongoose.connect('mongodb://localhost:27017/FloraView');
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/FloraView";
+
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 
